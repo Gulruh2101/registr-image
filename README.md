03505daab008a04aae5fc0ab2a88b020185602cb
Gulruh2101 <74314629+Gulruh2101@users.noreply.github.com>
2021-01-07 17:23:31 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# registr-image
+ 
---
+++ main.js
@@ -0,0 +1,26 @@
+alert('Assalomu alaykum loyihachamga hush kelibsiz!!🥰')
+
+const arrgument = []
+form.onsubmit = (masalan)=>{
+    masalan.preventDefault()
+    const path = rasm.value.replace(rasm.value.slice(0, rasm.value.indexOf('fakepath')+8), '.')
+    const object = {
+        name: ism.value,
+        about: about.value,
+        path: path
+    }
+    arrgument.push(object)
+    for(bir of arrgument){
+        const h2 = document.createElement("H2")
+        const p = document.createElement('P')
+        const img = document.createElement("IMG")
+        img.classList.add('photo')
+        h2.textContent = bir.name
+        p.textContent = bir.about
+        img.src = bir.path
+        shaxsiyMalumotlar.appendChild(h2)
+        shaxsiyMalumotlar.appendChild(p)
+        shaxsiyMalumotlar.appendChild(img)
+        console.log(shaxsiyMalumotlar)
+    }
+}
